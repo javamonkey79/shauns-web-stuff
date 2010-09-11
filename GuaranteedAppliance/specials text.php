f656b7fd5005d03643c53c60d1333a1bceae59fa
Shaun Elliott <javamonkey79@gmail.com>
2010-09-11 22:28:26 +0000

    * Added in most of the online inventory * Converted page over to php

---
+++ GuaranteedAppliance/specials text.php
@@ -0,0 +1,14 @@
+<?php
+
+	$text6 = "Maytag Dryer (White)";
+	$text7 = "Whirlpool Super Capacity Plus Washer & Dryer (White)";
+	$text8 = "Kenmore Ultra Fabric Care Washer & Dryer Set (White)";
+	$text9 = "Wards 20 CU.FT Side X Side Fridge (Almond) ";
+	$text10 = "Whirlpool Super Capacity Washer (Almond)";
+	$text11 = "Kenmore Washer Extra Capacity Plus (White)";
+	$text12 = "GE Slide In Range (Almond)";
+	$text13 = "Kenmore 90 series Super Capacity Plus (White)";
+	$text14 = "Amana 22 CU.FT. Fridge (Black)"; 
+	$text15 = "Kenmore Super Capacity Plus Dreyer (White) ";
+
+?>
