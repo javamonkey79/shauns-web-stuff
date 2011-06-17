69e9c84d5551b1c3db2efb97bdc7e956b97444f4
javamonkey79 <javamonkey79@4858bb00-e78f-44b7-8492-d342272e6317>
2011-06-17 21:38:51 +0000

    Added a simple UI

--- DO/src/main/java/javamonkey/web/ResourceGatherer.java
+++ DO/src/main/java/javamonkey/web/ResourceGatherer.java
@@ -1,13 +1,26 @@
 package javamonkey.web;
 
-import java.util.Iterator;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javamonkey.web.link.LinkParser;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import javamonkey.web.link.LinkParser;
 
 public class ResourceGatherer {
 
@@ -18,36 +31,98 @@ public class ResourceGatherer {
 	 * @throws Throwable
 	 */
 	public static void main(String[] args) throws Throwable {
-		String link = "http://live.nightowlgames.net/MyLairServer/DelegateServlet?act=loadPlayerJSON";
-		String cookieData = args.length == 0 || StringUtils.isBlank(args[0]) ? "JSESSIONID=115D9DDD70B4099A078814C5B5A2BD17; DUNGEON_OVERLORD_live=hc175qvn8b5vvincevil6st015; fbs_110362692359888=\"access_token=110362692359888%7C2.AQCj3VWHGA6Vac_h.3600.1308337200.1-507492112%7Cy3JFNqqzMaM9gPeMbizMajPYwB4&base_domain=nightowlgames.net&expires=1308337200&secret=N740koc7mEZivuudAJHgkA__&session_key=2.AQCj3VWHGA6Vac_h.3600.1308337200.1-507492112&sig=2a0c1d8beeb4f40cb824e44cc25dcbd4&uid=507492112\""
-				: args[0];
 
-		String linkPageSource = LINK_PARSER.getLinkPageSource(link, cookieData,
-				false);
+		JFrame mainFrame = new JFrame("DO Collector");
 
-		JSONObject playerData = new JSONObject(linkPageSource);
+		mainFrame.setLayout(new MigLayout("wrap 1"));
 
-		if (StringUtils.contains(linkPageSource, "not logged in.")) {
-			System.out.println("reset the cookie");
-			return;
-		}
+		final JTextArea cookieText = new JTextArea();
+		cookieText.setBorder(new LineBorder(Color.BLACK));
+		mainFrame.add(cookieText, "w 150px");
 
-		JSONArray dungeons = playerData.getJSONArray("dungeons");
-		for (int i = 0; i < dungeons.length(); i++) {
-			JSONObject dungeonInfo = dungeons.getJSONObject(i);
+		final JCheckBox collectAll = new JCheckBox("Collect All", true);
+		mainFrame.add(collectAll, "wrap");
 
-			String dungeonId = dungeonInfo.getString("id");
-			link = "http://live.nightowlgames.net/MyLairServer/DelegateServlet?act=loadDungeonJSON&id="
-					+ dungeonId;
+		final JCheckBox shockAll = new JCheckBox("Shock All", true);
+		mainFrame.add(shockAll, "wrap");
 
-			linkPageSource = LINK_PARSER.getLinkPageSource(link, cookieData,
-					false);
-			JSONObject dungeon = new JSONObject(linkPageSource);
+		JButton doIt = new JButton("Do It");
+		mainFrame.add(doIt, "wrap");
+		doIt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String cookieData = cookieText.getText();
 
-			shockAllWarlocks(cookieData, dungeonId, dungeon);
+				File cookieFile = new File("cookie.txt");
+				if (StringUtils.isNotBlank(cookieData)) {
+					try {
+						FileUtils.writeStringToFile(cookieFile, cookieData);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					try {
+						cookieData = FileUtils.readFileToString(cookieFile);
+					} catch (IOException e1) {
+						throw new RuntimeException(
+								"Could not load cookie file and nothing in text.");
+					}
+				}
 
-//			pickupResources(cookieData, dungeonId, dungeon);
-		}
+				System.out.println(cookieData);
+				System.out.println(shockAll.isSelected());
+				System.out.println(collectAll.isSelected());
+
+				String link = "http://live.nightowlgames.net/MyLairServer/DelegateServlet?act=loadPlayerJSON";
+
+				try {
+
+					// String cookieData = args.length == 0 ||
+					// StringUtils.isBlank(args[0])
+					// ?
+					// "JSESSIONID=115D9DDD70B4099A078814C5B5A2BD17; DUNGEON_OVERLORD_live=hc175qvn8b5vvincevil6st015; fbs_110362692359888=\"access_token=110362692359888%7C2.AQCj3VWHGA6Vac_h.3600.1308337200.1-507492112%7Cy3JFNqqzMaM9gPeMbizMajPYwB4&base_domain=nightowlgames.net&expires=1308337200&secret=N740koc7mEZivuudAJHgkA__&session_key=2.AQCj3VWHGA6Vac_h.3600.1308337200.1-507492112&sig=2a0c1d8beeb4f40cb824e44cc25dcbd4&uid=507492112\""
+					// : args[0];
+
+					String linkPageSource = LINK_PARSER.getLinkPageSource(link,
+							cookieData, false);
+					
+					System.out.println(linkPageSource);
+
+					JSONObject playerData = new JSONObject(linkPageSource);
+
+					if (StringUtils.contains(linkPageSource, "not logged in.")) {
+						System.out.println("reset the cookie");
+						return;
+					}
+
+					JSONArray dungeons = playerData.getJSONArray("dungeons");
+					for (int i = 0; i < dungeons.length(); i++) {
+						JSONObject dungeonInfo = dungeons.getJSONObject(i);
+
+						String dungeonId = dungeonInfo.getString("id");
+						link = "http://live.nightowlgames.net/MyLairServer/DelegateServlet?act=loadDungeonJSON&id="
+								+ dungeonId;
+
+						linkPageSource = LINK_PARSER.getLinkPageSource(link,
+								cookieData, false);
+						JSONObject dungeon = new JSONObject(linkPageSource);
+
+						if (shockAll.isSelected()) {
+							shockAllWarlocks(cookieData, dungeonId, dungeon);
+						}
+
+						if (collectAll.isSelected()) {
+							pickupResources(cookieData, dungeonId, dungeon);
+						}
+					}
+				} catch (Throwable throwable) {
+					throw new RuntimeException(throwable);
+				}
+			}
+		});
+
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setBounds(200, 200, 100, 200);
+		mainFrame.setVisible(true);
 	}
 
 	private static void shockAllWarlocks(String cookieData, String dungeonId,
