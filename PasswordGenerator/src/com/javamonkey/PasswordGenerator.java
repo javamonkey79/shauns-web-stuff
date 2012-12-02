969e4a692a13f0f314a4b88f2cf720b3940b96b2
javamonkey79 <javamonkey79@4858bb00-e78f-44b7-8492-d342272e6317>
2012-12-02 04:04:42 +0000

    now using an MD5 for password gen

--- PasswordGenerator/src/com/javamonkey/PasswordGenerator.java
+++ PasswordGenerator/src/com/javamonkey/PasswordGenerator.java
@@ -1,74 +1,39 @@
 package com.javamonkey;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class PasswordGenerator {
 
-	private static final int NUM_DIGITS = 10;
-	private static final int NUM_LETTERS = 26;
-	private static final int FIRST_LOWER_ASCII = 97;
-	private static final int FIRST_UPPER_ASCII = 65;
-	private static final int FIRST_NUMERIC_ASCII = 48;
-
-	public String generatePasswordForSeed(String seed) {
-		return generatePasswordForSeed(seed, -1);
-	}
-
 	public String generatePasswordForSeed(String seed, int hourToUse) {
 		GregorianCalendar gregorianCalendar = new GregorianCalendar();
 		int day = gregorianCalendar.get(Calendar.DAY_OF_MONTH);
 		int month = gregorianCalendar.get(Calendar.MONTH);
-		int hour = hourToUse == -1 ? gregorianCalendar
-				.get(Calendar.HOUR_OF_DAY) : hourToUse;
+		int hour = hourToUse == -1 ? gregorianCalendar.get(Calendar.HOUR_OF_DAY) : hourToUse;
 
-		String encrypted = "";
+		String encrypted = getMd5Hash(seed + day + month + hour);
 
-		char[] chars = seed.toCharArray();
-		for (char c : chars) {
-			int seedValue = hour + day + month + c;
-			int seedGroup = seedValue % 10;
-
-			char movedChar = getMovedCharForSeedGroup(seedValue, seedGroup);
-
-			if (movedChar == c) {
-				// wrap the seedgroups
-				seedGroup = c > 96 && seedGroup > 6 ? 6 : seedGroup;
-				seedGroup = c > 64 && c < 97 && seedGroup > 3 && seedGroup < 7 ? 3
-						: seedGroup;
-				seedGroup = c < 65 && seedGroup < 4 ? 9 : seedGroup;
-
-				movedChar = getMovedCharForSeedGroup(seedValue, seedGroup);
-			}
-
-			encrypted += movedChar;
-
-		}
-
-		return encrypted.substring(0,
-				encrypted.length() > 7 ? 8 : encrypted.length());
+		return encrypted.substring(0, 8);
 	}
 
-	private char getMovedCharForSeedGroup(int seedValue, int seedGroup) {
-		char movedChar = ' ';
-
-		if (seedGroup < 4) {
-			// num
-			int movement = seedValue % FIRST_NUMERIC_ASCII;
-
-			movedChar = (char) ((movement % NUM_DIGITS) + FIRST_NUMERIC_ASCII);
-		} else if (seedGroup < 7) {
-			// upper
-			int movement = seedValue % FIRST_UPPER_ASCII;
-
-			movedChar = (char) ((movement % NUM_LETTERS) + FIRST_UPPER_ASCII);
-		} else {
-			// lower
-			int movement = seedValue % 97;
-
-			movedChar = (char) ((movement % NUM_LETTERS) + FIRST_LOWER_ASCII);
+	private String getMd5Hash(String input) {
+		MessageDigest messageDigest;
+		try {
+			messageDigest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
 		}
-		return movedChar;
-	}
 
+		messageDigest.reset();
+		messageDigest.update(input.getBytes(Charset.forName("UTF8")));
+		final byte[] digest = messageDigest.digest();
+		BigInteger bigInt = new BigInteger(1, digest);
+		String hashtext = bigInt.toString(16);
+
+		return hashtext.toUpperCase();
+	}
 }
